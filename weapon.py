1226849c8e905e48774748310fef193e1c5cd699
a <a@a.a>
2023-01-17 19:21:05 +0300

    +худ +оружие крутится и стреляет

--- weapon.py
+++ weapon.py
@@ -6,13 +6,11 @@ import pygame
 
 from main import load_image
 
-class Weapon(pygame.sprite.Sprite):
-    """
-    оружия:
-    дальники: пистолет, шелли, байрон ...
-    ближники: катана, топор, палка ...
-    написаны в порядке убывания, типо катана самый мощный ближник и тп
-    """
+
+class Weapon(pygame.sprite.Sprite):  # каркас мили и дальнего оружия
+    AUTO = 'AUTO'
+    SINGLE = 'SINGLE'
+    BURST = 'BURST'  # типы стрельбы
 
     def __init__(self, x, y, *groups):
         super().__init__(*groups)
@@ -21,25 +19,27 @@ class Weapon(pygame.sprite.Sprite):
         self.ammo_max: int = 0  # сколько можно хранить патрон для этого оружия(без учета магаза)
         # (мб переделать типа патроны не индивидуально хранятся, а в инвентаре)
         self.all_ammo_current: int = 0  # скока ща всего патрон (без учета магаза)
-        self.fire_rate: float = 0.0  # темп
         self.spread: float = 0.0  # разброс
+        self.reload_time: float = 0.0  # время перезарядлки оружия
         self.attack_sound: str = ''  # путь до звука
         self.name: str = 'NONE'
         self.rarity = None  # типа редкое\легендарное\эпичное и тп TODO: ДОБАВИТЬ РЕДКОСТЬ
-        self.cooldown: float = 0.0  # кулдаун собсна В СЕКУНДАХ(через time делать потомучт буду)
+        self.shoot_type = Weapon.SINGLE
+        self.cooldown: float = 0.0  # темп собсна В СЕКУНДАХ(через time делать потомучт буду)
         self.can_shoot: bool = True  # если нет патрон/перезарядка/кулдаун, то фолс
+        self.reloading = False  # чтоб игрок не спамил перезарядкой в момент перезарядки
         self.frames = []
-        self.cur_frame = 5
+        self.cur_frame = 5  # номер кадра с картинкой оружия
         self.cut_sheet(load_image("weapons/weapons1.png"), 10, 20)
         self.image = pygame.transform.scale(self.frames[self.cur_frame], (98, 44))
+        self.angle: int = 0  # угол поворота
 
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.pos = self.rect.topleft
 
-    def cooldown_timer(self):
-        self.can_shoot = False
-        t_timer = Timer(self.cooldown, self.cooldown_finish)
+    def timer(self, time, func):
+        t_timer = Timer(time, func)
         t_timer.start()
 
     def cooldown_finish(self):
@@ -83,6 +83,9 @@ class Weapon(pygame.sprite.Sprite):
         user.available_weapons.append(self)
         user.equipped_weapon = len(user.available_weapons) - 1
 
+    def rot_center(self, angle):
+        self.image = pygame.transform.rotate(self.image, angle)
+
 
 class Gun(Weapon):
     """
@@ -91,13 +94,44 @@ class Gun(Weapon):
 
     def __init__(self, x, y, *groups):
         super().__init__(x, y, *groups)
-        self.curr_mag_ammo = 15
+        self.curr_mag_ammo: int = 15  # сколько патрон ща в магазе
+        self.mag_capacity: int = 15  # сколько влазит в магаз
+        self.ammo_max: int = 100  # сколько можно хранить патрон для этого оружия(без учета магаза)
+        # TODO: (мб переделать типа патроны не индивидуально хранятся, а в инвентаре)
+        self.all_ammo_current: int = 32  # скока ща всего патрон (без учета магаза)
+        self.spread: float = 6.8  # разброс
+        self.reload_time = 3.0
+        self.attack_sound: str = ''  # путь до звука
+        self.name: str = 'DEV GUN'
+        self.rarity = None  # типа редкое\легендарное\эпичное и тп TODO: ДОБАВИТЬ РЕДКОСТЬ
+        self.shoot_type = Weapon.BURST
+        self.burst_count_max = 4  # скок патров за берст улетает
+        self.burst_curr = 4  # скок берстом еще вылетит
+        self.burst_timer: float = 0.3  # кулдаун между кучковыми выстрелами
+        self.cooldown: float = 0.025  # кулдаун собсна В СЕКУНДАХ(через time делать потомучт буду)
+        self.can_shoot: bool = True  # если нет патрон/перезарядка/кулдаун, то фолс
+        self.burst_can_shoot: bool = True
+
+        self.cur_frame = 5  # номер кадра с картинкой оружия
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (98, 44))
         self.no_ammo_sound: str = ''  # звук отсутствия патрон
 
+    def burst_cooldown_finish(self):
+        self.burst_curr = self.burst_count_max
+        self.burst_can_shoot = True
+
     def shoot(self, user, mouse_pos):
-        print('SHOOT')
         # current_time = pygame.time.get_ticks()  # а оно надо?
-        if self.can_shoot and self.curr_mag_ammo > 0:
+        if self.can_shoot and self.burst_can_shoot and self.curr_mag_ammo > 0:
+            if self.shoot_type == Weapon.BURST:
+                if self.burst_curr == 0:
+                    self.burst_can_shoot = False
+                    self.timer(self.burst_timer, self.burst_cooldown_finish)
+                else:
+                    self.burst_curr -= 1
+            elif self.shoot_type == Weapon.SINGLE:
+                self.burst_can_shoot = False
+                self.timer(self.burst_timer, self.burst_cooldown_finish)
             self.curr_mag_ammo -= 1  # минус патрон
             direction = (mouse_pos[0] - user.pos[0], mouse_pos[1] - user.pos[1]) \
                 if mouse_pos != user.pos else (1, 1)  # куда стреляем
@@ -106,9 +140,95 @@ class Gun(Weapon):
 
             # спавним пулю и передаем ей юзера, направление, скорость, длительность полета, цвет?(поменять на пнг)
             # и спрайт-группу
+            self.can_shoot = False
+            self.timer(self.cooldown, self.cooldown_finish)
+            print('SHOOT', self.curr_mag_ammo, self.all_ammo_current)
             return user.pos, super().normalize_vector(angled_direction), 5, 1000
         if self.curr_mag_ammo == 0:
             return print('NO AMMO')  # типа self.no_ammo_sound играть и мб как-то игроку показывать "NO AMMO"
+
+    def reload(self):
+        if self.curr_mag_ammo == self.mag_capacity:
+            return
+        if self.reloading is False and self.all_ammo_current > 0:
+            print('RELOADIN')
+            self.reloading = True
+            self.can_shoot = False
+            self.timer(self.reload_time, self.reload)
+        else:
+            tmp = (self.mag_capacity - self.curr_mag_ammo) % self.all_ammo_current
+            self.curr_mag_ammo += tmp
+            self.all_ammo_current -= tmp
+            print('RELOADIN FINISHED', self.curr_mag_ammo, self.all_ammo_current)
+            self.reloading = False
+            self.can_shoot = True
+
+
+class MachineGun(Gun):
+    def __init__(self, x, y, *groups):
+        super().__init__(x, y, *groups)
+        self.curr_mag_ammo: int = 15
+        self.mag_capacity: int = 15
+        self.ammo_max: int = 100
+        self.all_ammo_current: int = 32
+        self.spread: float = 6.8
+        self.reload_time = 3.0
+        self.attack_sound: str = ''
+        self.name: str = 'machine gun 1'
+        self.rarity = None
+        self.shoot_type = Weapon.AUTO
+        self.cooldown: float = 0.2
+        self.can_shoot: bool = True
+
+        self.cur_frame = 6
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (98, 44))
+        self.no_ammo_sound: str = ''
+
+
+class PistolLikeGun(Gun):
+    def __init__(self, x, y, *groups):
+        super().__init__(x, y, *groups)
+        self.curr_mag_ammo: int = 15
+        self.mag_capacity: int = 15
+        self.ammo_max: int = 100
+        self.all_ammo_current: int = 32
+        self.spread: float = 6.8
+        self.reload_time = 3.0
+        self.attack_sound: str = ''
+        self.name: str = 'machine gun 1'
+        self.rarity = None
+        self.shoot_type = Weapon.SINGLE
+        self.cooldown: float = 0.2
+        self.can_shoot: bool = True
+
+        self.cur_frame = 6
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (98, 44))
+        self.no_ammo_sound: str = ''
+
+
+class BurstGun(Gun):
+    def __init__(self, x, y, *groups):
+        super().__init__(x, y, *groups)
+        self.curr_mag_ammo: int = 15
+        self.mag_capacity: int = 15
+        self.ammo_max: int = 100
+        self.all_ammo_current: int = 32
+        self.spread: float = 6.8
+        self.reload_time = 3.0
+        self.attack_sound: str = ''
+        self.name: str = 'machine gun 1'
+        self.rarity = None
+        self.shoot_type = Weapon.BURST
+        self.burst_count_max = 4
+        self.burst_curr = 4
+        self.burst_timer: float = 0.3
+        self.cooldown: float = 0.025
+        self.can_shoot: bool = True
+        self.burst_can_shoot: bool = True
+
+        self.cur_frame = 6
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (98, 44))
+        self.no_ammo_sound: str = ''
 
 
 class Melee(Weapon):
